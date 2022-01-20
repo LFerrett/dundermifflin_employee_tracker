210e852d9f4a4ebe297c56f6a7c8c8473d3cd816
“Lauren <lferrett@gmail.com>
2022-01-19 22:46:31 -0500

    Added screenshot.

--- server.js
+++ server.js
@@ -6,24 +6,18 @@ const cTable = require("console.table");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connect to Office database
 const db = mysql.createConnection(
   {
     host: "localhost",
-    // MySQL username,
     user: "root",
-    // MySQL password
     password: "Mattie123",
     database: "office_db",
   }
-  // console.log(`Connected to the office_db database.`)
 );
 
-// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
